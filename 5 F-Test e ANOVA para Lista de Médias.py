6bfbdaa857dcd520453a6f55842ff842bd287b46
Diego Rodrigues <diego.mrodrigues11@gmail.com>
2020-03-20 14:09:04 -0300

    Student-t Test entre Lista de Medias

--- 5 F-Test e ANOVA para Lista de Médias.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import math
-import numpy as np
-import scipy.stats as stats
-
-np.random.seed(seed=42)
-
-
---
+++ 5 Student-t Test entre Lista de Médias.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import math
+import numpy as np
+import scipy.stats as stats
+
+np.random.seed(seed=42)
+
+# Condições:
+# 1. Independencia
+#   entre grupos
+#   dentro dos grupos
+# 2. Normalidade
+# 3. A variancia entre os grupos 
+#    deve ser semelhante
+
+# Amostras
+# (mean, n)
+samples = [
+    (5.07, 41),
+    (6.76, 331)
+] #@param
+
+# Parametros do Experimento
+ci = 0.95 #@param
+alpha = 1 - ci
+
+df_t = min(map(lambda e: e[1], samples)) - 1
+df_k = len(samples) - 1
+df_e = df_t - df_k
+
+
