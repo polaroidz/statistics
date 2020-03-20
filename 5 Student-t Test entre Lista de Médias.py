4e52e2221b3b4688d5a4327b5696113b928e2f9c
Diego Rodrigues <diego.mrodrigues11@gmail.com>
2020-03-20 14:12:38 -0300

    Student-t Test entre Lista de Medias

--- 5 Student-t Test entre Lista de Médias.py
+++ 5 Student-t Test entre Lista de Médias.py
@@ -17,6 +17,7 @@ np.random.seed(seed=42)
 # (mean, n)
 samples = [
     (5.07, 41),
+    (7.0, 200),
     (6.76, 331)
 ] #@param
 
@@ -25,7 +26,12 @@ ci = 0.95 #@param
 alpha = 1 - ci
 
 df_t = min(map(lambda e: e[1], samples)) - 1
-df_k = len(samples) - 1
+
+k = len(samples) * (len(samples) - 1) / 2
+df_k = k - 1
+
 df_e = df_t - df_k
+
+alpha_k = alpha / k
 
 
